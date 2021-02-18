3ef8c2c4689e39cbaadae94a5d36691c9dd18a5a
billionray <8900581@qq.com>
2021-02-18 18:00:55 +0800

    更正错误

--- teSt.py
+++ teSt.py
@@ -4,14 +4,14 @@ from Services import service
 datetime = time.strftime("%Y-%m-%d", time.localtime())  # 获取日期 YY-MM-DD
 日期=datetime
 username = os.environ['USERNAME']
-password= os.environ('PASSWORD')
-mobile=os.environ('mobile')
-homemobile=os.environ('HOMEMOBILE')
-gpslocation=os.environ('GPS')
-lat=os.environ('LAT')#小数点后五位
-lon=os.environ('LON')  #小数点后五位
-my_user=os.environ('USER')
-my_sender=os.environ('SENDER')
-SMTPdomain=os.environ('SMTPDOMAIN')
-SMTPauth=os.environ('SMTPAUTH')
+password= os.environ['PASSWORD']
+mobile=os.environ['mobile']
+homemobile=os.environ['HOMEMOBILE']
+gpslocation=os.environ['GPS']
+lat=os.environ['LAT']#小数点后五位
+lon=os.environ['LON']  #小数点后五位
+my_user=os.environ['USER']
+my_sender=os.environ['SENDER']
+SMTPdomain=os.environ['SMTPDOMAIN']
+SMTPauth=os.environ['SMTPAUTH']
 service(username,password,mobile,homemobile,gpslocation,lat,lon,my_user,my_sender,SMTPdomain,SMTPauth,datetime)
